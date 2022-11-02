978ada354a3272b58d31fd0b2b2f78e9a45d0229
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-11-02 00:07:38 -0700

    Create for loop to render date, icon, temperature, wind, and humidity to the proper container. Change date font size and weight of forecast dates.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,32 @@ var apiKey = "82273e2ffbfbb5621069d2976422e5ca";
 var searchButton = document.querySelector(".search-button");
 var forecastEl = document.querySelector(".forecast-row").children;
 var city = "San Diego";
+var forecastArray = [];
 // Query Select Elements
 
 // Five day forecast: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // Found going here: https://openweathermap.org/forecast5#name5
 
-var fetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-        .then(response => response.json())
-        .then(result => {
-            var cityName = result.city.name;
-            for (let i = 0; i < result.list.length; i++) {
-                const element = result.list[i].dt_txt;
-                var hour = moment(element, "YYYY-MM-DD hh:mm:ss").format("HH");
-                if (hour === "12") {
-                    var windSpeed = result.list[i].wind.speed;
-                    var temp = result.list[i].main.temp;
-                    var icon = result.list[i].weather[0].icon;
-                    var humidity = result.list[i].main.humidity;
-                    var dt_text = result.list[i].dt_txt;
-                    var date = moment(dt_text, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YY");
-                    console.log(windSpeed);
-                    console.log(temp);
-                    console.log(icon);
-                    console.log(humidity);
-                    console.log(date);
-                }
+var forecast = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+    .then(response => response.json())
+    .then(result => {
+        var cityName = result.city.name;
+        for (let i = 0; i < result.list.length; i++) {
+            const element = result.list[i].dt_txt;
+            var hour = moment(element, "YYYY-MM-DD hh:mm:ss").format("HH");
+            if (hour === "12") {
+                var windSpeed = `Wind: ${result.list[i].wind.speed} MPH`;
+                var temp = `Temp: ${result.list[i].main.temp} \u2109`;
+                var icon = result.list[i].weather[0].icon;
+                var humidity = `Humidity: ${result.list[i].main.humidity}%`;
+                var date = moment(result.list[i].dt_txt, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YY");
+                forecastArray.push([date, icon, temp, windSpeed, humidity]);
             }
         }
-            )
-        .catch(error => console.log('error', error)); 
+        forecastLoop();
+    }
+    )
+    .catch(error => console.log('error', error));
 
 // Find lat/lon by zip code: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
 // May use city name for current weather: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -42,3 +39,17 @@ var fetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&ap
 // local storage buttons can run through the function
 
 // Use local storage array to dynamically create buttons after the search button is clicked.
+
+var forecastLoop = function () {
+    for (let x = 0; x < forecastEl.length; x++) {
+        for (let y = 0; y < forecastArray.length; y++) {
+            var El = forecastEl[x].children[y];
+            var text = forecastArray[x][y];
+            console.log(El);
+            if (y === 1) {
+                El.setAttribute("src", `http://openweathermap.org/img/w/${text}.png`);
+            }
+            else{El.textContent = text;}
+        }
+    }
+}
