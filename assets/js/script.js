4353d5c824eebe3b2ab85b0569eb09a04b6adb0e
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-11-03 00:30:17 -0700

    Create event listener for search button, create local storage items for each search as long as it hasn't been searched before. Implement if statements to differentiate search button from local storage buttons. For loop to render local storage buttons upon refresh.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,125 @@
 var apiKey = "82273e2ffbfbb5621069d2976422e5ca";
 var searchButton = document.querySelector(".search-button");
+var searchBar = document.querySelector(".search-bar");
 var forecastEl = document.querySelector(".forecast-row").children;
-var city = "San Diego";
-var forecastArray = [];
+var city = "Salinas";
+var inputEl = document.querySelector("input");
+
+var mainEl = document.querySelector("main");
+var currentEl = mainEl.children[1].children[0].children;
+var newButtons = mainEl.children[0];
 // Query Select Elements
+currentWeather(city);
+fivedayForecast(city);
 
-// Five day forecast: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-// Found going here: https://openweathermap.org/forecast5#name5
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
-var forecast = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-    .then(response => response.json())
-    .then(result => {
-        var cityName = result.city.name;
-        for (let i = 0; i < result.list.length; i++) {
-            const element = result.list[i].dt_txt;
-            var hour = moment(element, "YYYY-MM-DD hh:mm:ss").format("HH");
-            if (hour === "12") {
-                var windSpeed = `Wind: ${result.list[i].wind.speed} MPH`;
-                var temp = `Temp: ${result.list[i].main.temp} \u2109`;
-                var icon = result.list[i].weather[0].icon;
-                var humidity = `Humidity: ${result.list[i].main.humidity}%`;
-                var date = moment(result.list[i].dt_txt, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YY");
-                forecastArray.push([date, icon, temp, windSpeed, humidity]);
+function currentWeather(city) {
+
+    var current = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=82273e2ffbfbb5621069d2976422e5ca&units=imperial`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            var windSpeed = `Wind: ${result.wind.speed} MPH`;
+            var temp = `Temp: ${result.main.temp} \u2109`;
+            var icon = result.weather[0].icon;
+            var humidity = `Humidity: ${result.main.humidity}%`;
+            var date = moment().format("MM/DD/YY");
+            var topRow = `${city}, ${date} <img src="http://openweathermap.org/img/w/${icon}.png" alt="">`;
+            currentEl[0].innerHTML = topRow;
+            currentEl[1].textContent = temp;
+            currentEl[2].textContent = windSpeed;
+            currentEl[3].textContent = humidity;
+        })
+        .catch(error => console.log('error', error));
+}
+
+function fivedayForecast(city) {
+    var forecastArray = [];
+    var forecast = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+        .then(response => response.json())
+        .then(result => {
+            var cityName = result.city.name;
+            var count = 0;
+            for (let i = 0; i < result.list.length; i++) {
+                const element = result.list[i].dt_txt;
+                var hour = moment(element, "YYYY-MM-DD hh:mm:ss").format("HH");
+                var tomorrow = moment(element, "YYYY-MM-DD hh:mm:ss").format("DD");
+                var today = moment().format("DD");
+                // Using the hour to select the data might not be the best. 
+                if (hour === "12" && parseInt(today) < parseInt(tomorrow) || i === result.list.length - 1) {
+                    if (count === 5) {
+                        forecastLoop();
+                    } else {
+                        var windSpeed = `Wind: ${result.list[i].wind.speed} MPH`;
+                        var temp = `Temp: ${result.list[i].main.temp} \u2109`;
+                        var icon = result.list[i].weather[0].icon;
+                        var humidity = `Humidity: ${result.list[i].main.humidity}%`;
+                        var date = moment(result.list[i].dt_txt, "YYYY-MM-DD hh:mm:ss").format("MM/DD/YY");
+                        forecastArray.push([date, icon, temp, windSpeed, humidity]);
+                    }
+                    count += 1;
+                }
             }
+            forecastLoop(forecastArray);
         }
-        forecastLoop();
-    }
-    )
-    .catch(error => console.log('error', error));
+        )
+        .catch(error => console.log('error', error));
+}
 
-// Find lat/lon by zip code: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
 // May use city name for current weather: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// May use lat/long cor current weather: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-// Use search information to create URL parameters. Use these to search the API.
-
 // Fetch API. Create a separate function so the original search button and
 // local storage buttons can run through the function
-
 // Use local storage array to dynamically create buttons after the search button is clicked.
 
-var forecastLoop = function () {
+var forecastLoop = function (forecastArray) {
     for (let x = 0; x < forecastEl.length; x++) {
         for (let y = 0; y < forecastArray.length; y++) {
             var El = forecastEl[x].children[y];
             var text = forecastArray[x][y];
-            console.log(El);
             if (y === 1) {
                 El.setAttribute("src", `http://openweathermap.org/img/w/${text}.png`);
             }
-            else{El.textContent = text;}
+            else { El.textContent = text; }
         }
     }
+}
+
+var buttonArray = [];
+var storageCount = localStorage.length;
+var searchEntry = function (event) {
+    event.preventDefault();
+    if (event.target.textContent !== "Search" && event.target.tagName === "BUTTON") {
+        value = event.target.textContent;
+        console.log(value);
+        currentWeather(value);
+        fivedayForecast(value);
+    }
+    if (event.target.textContent === "Search") {
+        var value = inputEl.value.trim();
+        if (buttonArray.indexOf(`${value}`) !== -1) {
+            currentWeather(value);
+            fivedayForecast(value);
+            return;
+        }
+        buttonArray.push(value);
+        newButtons.appendChild(document.createElement("button")).className = `w-100 mt-2 btn btn-info search-button`;
+        searchBar.children[0].lastChild.textContent = value;
+        localStorage.setItem(`savedButton${storageCount}`, value);
+        storageCount += 1;
+        console.log(value);
+        currentWeather(value);
+        fivedayForecast(value);
+    }
+}
+
+searchBar.addEventListener("click", searchEntry);
+
+for (let t = 0; t < localStorage.length; t++) {
+    const element = localStorage.getItem(`savedButton${t}`);
+    newButtons.appendChild(document.createElement("button")).className = `w-100 mt-2 btn btn-info search-button`;
+    searchBar.children[0].lastChild.textContent = element;
+    buttonArray.push(element);
 }
